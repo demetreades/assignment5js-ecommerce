185324e4d1efa2d1dcc1a72fe2d376ba61d3861d
demetreades <demetreade@protonmail.com>
2021-09-10 12:08:29 +0300

    updateProduct added

--- server/src/controllers/products/index.js
+++ server/src/controllers/products/index.js
@@ -2,10 +2,12 @@ const getProducts = require('./getProducts');
 const getProductById = require('./getProductById');
 const createProduct = require('./createProduct');
 const deleteProduct = require('./deleteProduct');
+const updateProduct = require('./updateProduct');
 
 module.exports = {
   getProducts,
   getProductById,
   createProduct,
   deleteProduct,
+  updateProduct,
 };
