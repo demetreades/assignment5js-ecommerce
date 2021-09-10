a04284238fa77a7d937c5f9eebaf8fb27142bba8
demetreades <demetreade@protonmail.com>
2021-09-10 12:12:46 +0300

    getUsers added

---
+++ server/src/controllers/users/getUsers/index.js
@@ -0,0 +1,18 @@
+// const { StatusCodes } = require('http-status-codes');
+const asyncHandler = require('express-async-handler');
+const { logger } = require('../../../utils');
+const { User } = require('../../../models');
+
+/**
+ * @description  Fetch all users
+ * @route        GET /api/users
+ * @access       Private/Admin
+ */
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+
+  logger.info(`GET ALL ${users.length} USERS`);
+  res.json(users);
+});
+
+module.exports = getUsers;
