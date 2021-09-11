bb897d9dc231d1297eebb06c296e34077064678e
demetreades <demetreade@protonmail.com>
2021-09-11 13:05:44 +0300

    categories import added

--- server/src/config/DATA/index.js
+++ server/src/config/DATA/index.js
@@ -1,7 +1,9 @@
 const products = require('./products');
+const categories = require('./categories');
 const users = require('./users');
 
 module.exports = {
   products,
+  categories,
   users,
 };
