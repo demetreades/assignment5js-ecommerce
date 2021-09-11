f52929676e470927467bd323fd3a3343167eadea
demetreades <demetreade@protonmail.com>
2021-09-11 13:06:03 +0300

    categories DATA added

---
+++ server/src/config/DATA/categories.js
@@ -0,0 +1,25 @@
+const categories = [
+  {
+    name: 'shoes',
+    image: '/img/cat/shoes.jpg',
+    description: 'asdasdsadsad',
+    isActive: true,
+    products: [],
+  },
+  {
+    name: 'shirts',
+    image: '/img/cat/shirts.jpg',
+    description: 'asdasdasd',
+    isActive: true,
+    products: [],
+  },
+  {
+    name: 'pants',
+    image: '/img/cat/pants.jpg',
+    description: 'asdasdasd',
+    isActive: false,
+    products: [],
+  },
+];
+
+module.exports = categories;
