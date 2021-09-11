824d0fe562bbf649c56ebd67540683c46937670b
demetreades <demetreade@protonmail.com>
2021-09-11 13:07:24 +0300

    getCategories added

---
+++ server/src/controllers/categories/getCategories/index.js
@@ -0,0 +1,21 @@
+// const { StatusCodes } = require('http-status-codes');
+const asyncHandler = require('express-async-handler');
+const { logger } = require('../../../utils');
+const { Category } = require('../../../models');
+
+/**
+ * @description  Fetch all categories
+ * @route        GET /api/categories
+ * @access       Private/Admin
+ */
+const getCategories = asyncHandler(async (req, res) => {
+  const categories = await Category.find({});
+
+  logger.info(`GET ALL ${categories.length} CATEGORIES`);
+  res.json({
+    total: categories.length,
+    categories,
+  });
+});
+
+module.exports = getCategories;
