dbc00bc120ab9898e75b6e648f1807485507da21
demetreades <demetreades@protonmail.com>
2021-09-16 15:04:02 +0300

    logging level option added

--- server/src/utils/logger/index.js
+++ server/src/utils/logger/index.js
@@ -1,11 +1,9 @@
 const pino = require('pino');
 
-// expressLogger({ logger });
-// app.use(expressLogger);
-
 const logger = pino({
   prettyPrint: process.env.NODE_ENV || 'development',
   colorize: process.env.NODE_ENV || 'development',
+  //   level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
 });
 
 module.exports = logger;
